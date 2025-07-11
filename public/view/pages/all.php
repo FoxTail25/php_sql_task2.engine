3cd98403bfa1f73fecd39090dc90510521066974
Grigoriy <foxtail25@gmail.com>
2025-07-11 09:55:06 +0300

    авто генерация ссылок на категории

--- public/view/pages/all.php
+++ public/view/pages/all.php
@@ -1,10 +1,13 @@
 <?php
-$content = "<div>
-<a/ href=\"page/first-category\">first category</a>
-</div>";
-$content .= "<div>
-<a/ href=\"page/second-category\">second category</a>
-</div>";
+$query = "SELECT * FROM categories";
+$res = mysqli_query($site_base_link, $query);
+for($data = []; $row = mysqli_fetch_assoc($res); $data[] = $row);
+$content = '<div>';
+foreach($data as $elem) {
+    $content .= "<a href=\"page/$elem[slug]\">$elem[name]</a><br/>";
+};
+$content .="</div>";
+
 $page = ['title' => 'home title', 'content' => $content];
 return $page;
 ?>
